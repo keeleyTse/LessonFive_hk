88ddb7765cd475e45929fd70addd3dee38cdf874
Administrator <Administrator@PC-201701011205>
2018-06-06 23:50:49 +0800

    hk

---
+++ src/test/java/com/keeley/LessonFive_hk/BroserUtil.java
@@ -0,0 +1,11 @@
+package com.keeley.LessonFive_hk;
+
+import org.openqa.selenium.WebDriver;
+
+public class BroserUtil {
+	WebDriver driver;
+	public  WebDriver SetUpBrowser(String browser) {
+		return driver;
+	}
+
+}
---
+++ src/test/java/com/keeley/LessonFive_hk/TestCase.java
@@ -0,0 +1,10 @@
+package com.keeley.LessonFive_hk;
+
+import org.testng.annotations.Test;
+
+public class TestCase {
+	@Test
+	public void TestDemo() {
+		System.out.println("TestPrint: LessonFive_hk~~");
+	}
+}
