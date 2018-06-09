2daeedacc5ab932326fb35a2b2c15e33d5904413
Administrator <Administrator@PC-201701011205>
2018-06-10 00:26:47 +0800

    ---

--- src/test/java/com/keeley/RerunDemo/TestNGRetry.java
+++ src/test/java/com/keeley/RerunDemo/TestNGRetry.java
@@ -3,18 +3,17 @@ package com.keeley.RerunDemo;
 import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
 
-public class TestNGRetry implements IRetryAnalyzer {
+public class TestNGRetry implements IRetryAnalyzer{
 	private int retryCount = 1;
-	private int maxRetryCount =2;
-	
-	@Override
-	public boolean retry(ITestResult result) {
-		if (retryCount < maxRetryCount) {
-			retryCount++;
-			return true;
-		}else {
-			return false;
-		}
-	}
+    private int maxRetryCount = 3;   // 失败后重跑的次数
 
+    @Override
+    public boolean retry(ITestResult result) {
+        if (retryCount < maxRetryCount) {
+            retryCount++;
+            return true;
+        }else { 
+        	return false;	
+        }
+     }
 }
